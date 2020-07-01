408878fac62a649d2f2880ea6c09682819ea7729
adoube00 <1161524229@qq.com>
2020-07-01 15:36:52 +0800

    接收kafka消息

--- src/main/java/com/demo/test/KafkaDemo.java
+++ src/main/java/com/demo/test/KafkaDemo.java
@@ -1,53 +1,57 @@
-package com.demo.test;
-
-//import com.demo.trigger.CustomProcessingTimeTrigger;
-import org.apache.flink.api.common.functions.RichMapFunction;
-import org.apache.flink.api.common.restartstrategy.RestartStrategies;
-import org.apache.flink.api.common.serialization.SimpleStringSchema;
-import org.apache.flink.configuration.Configuration;
-import org.apache.flink.runtime.jobgraph.JobVertex;
-import org.apache.flink.streaming.api.TimeCharacteristic;
-import org.apache.flink.streaming.api.datastream.AllWindowedStream;
-import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.datastream.DataStreamSource;
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.sink.PrintSinkFunction;
-import org.apache.flink.streaming.api.windowing.time.Time;
-import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumerBase;
-
-import java.util.Properties;
-
-public class KafkaDemo {
-
-    public static void main(String[] args) throws Exception {
-
-        // set up the streaming execution environment
-        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        //默认情况下，检查点被禁用。要启用检查点，请在StreamExecutionEnvironment上调用enableCheckpointing(n)方法，
-        // 其中n是以毫秒为单位的检查点间隔。每隔5000 ms进行启动一个检查点,则下一个检查点将在上一个检查点完成后5秒钟内启动
-
-        env.enableCheckpointing(500);
-        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
-        Properties properties = new Properties();
-        properties.setProperty("bootstrap.servers", "localhost:9092");//kafka的节点的IP或者hostName，多个使用逗号分隔
-        properties.setProperty("zookeeper.connect", "localhost:2181");//zookeeper的节点的IP或者hostName，多个使用逗号进行分隔
-        properties.setProperty("group.id", "test-consumer-group");//flink consumer flink的消费者的group.id
-        System.out.println("11111111111");
-        FlinkKafkaConsumer010<String> myConsumer = new FlinkKafkaConsumer010<String>("test", new org.apache.flink.api.common.serialization.SimpleStringSchema(), properties);
-        // FlinkKafkaConsumer010<String> myConsumer = new FlinkKafkaConsumer010<String>("test",new SimpleStringSchema(),properties);//test0是kafka中开启的topic
-        myConsumer.assignTimestampsAndWatermarks(new CustomWatermarkEmitter());
-        DataStream<String> keyedStream = env.addSource(myConsumer);//将kafka生产者发来的数据进行处理，本例子我进任何处理
-        System.out.println("2222222222222");
-        keyedStream.print();//直接将从生产者接收到的数据在控制台上进行打印
-        // execute program
-        System.out.println("3333333333333");
-        env.execute("Flink Streaming Java API Skeleton");
-
-
-    }
-
-
-
-}
+//package com.demo.test;
+//
+////import com.demo.trigger.CustomProcessingTimeTrigger;
+//import org.apache.flink.api.common.functions.RichMapFunction;
+//import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+//import org.apache.flink.api.common.serialization.SimpleStringSchema;
+//import org.apache.flink.configuration.Configuration;
+//import org.apache.flink.runtime.jobgraph.JobVertex;
+//import org.apache.flink.streaming.api.TimeCharacteristic;
+//import org.apache.flink.streaming.api.datastream.AllWindowedStream;
+//import org.apache.flink.streaming.api.datastream.DataStream;
+//import org.apache.flink.streaming.api.datastream.DataStreamSource;
+//import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+//import org.apache.flink.streaming.api.functions.sink.PrintSinkFunction;
+//import org.apache.flink.streaming.api.windowing.time.Time;
+//import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+//import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010;
+//import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumerBase;
+//
+//import java.util.Properties;
+//
+//public class KafkaDemo {
+//
+//    public static void main(String[] args) {
+//
+//        // set up the streaming execution environment
+//        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+//        //默认情况下，检查点被禁用。要启用检查点，请在StreamExecutionEnvironment上调用enableCheckpointing(n)方法，
+//        // 其中n是以毫秒为单位的检查点间隔。每隔5000 ms进行启动一个检查点,则下一个检查点将在上一个检查点完成后5秒钟内启动
+//
+//        env.enableCheckpointing(500);
+//        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+//        Properties properties = new Properties();
+//        properties.setProperty("bootstrap.servers", "localhost:9092");//kafka的节点的IP或者hostName，多个使用逗号分隔
+//        properties.setProperty("zookeeper.connect", "localhost:2181");//zookeeper的节点的IP或者hostName，多个使用逗号进行分隔
+//        properties.setProperty("group.id", "test-consumer-group");//flink consumer flink的消费者的group.id
+//        System.out.println("11111111111");
+//        FlinkKafkaConsumer010<String> myConsumer = new FlinkKafkaConsumer010<String>("test", new org.apache.flink.api.common.serialization.SimpleStringSchema(), properties);
+//        // FlinkKafkaConsumer010<String> myConsumer = new FlinkKafkaConsumer010<String>("test",new SimpleStringSchema(),properties);//test0是kafka中开启的topic
+//        myConsumer.assignTimestampsAndWatermarks(new CustomWatermarkEmitter());
+//        DataStream<String> keyedStream = env.addSource(myConsumer);//将kafka生产者发来的数据进行处理，本例子我进任何处理
+//        System.out.println("2222222222222");
+//        keyedStream.print();//直接将从生产者接收到的数据在控制台上进行打印
+//        // execute program
+//        System.out.println("3333333333333");
+//        try {
+//            env.execute("Flink Streaming Java API Skeleton");
+//        } catch (Exception e) {
+//            System.out.println(e);
+//            e.printStackTrace();
+//        }
+//
+//    }
+//
+//
+//
+//}
--- src/main/java/com/demo/test/KafkaTest.java
+++ src/main/java/com/demo/test/KafkaTest.java
@@ -1,55 +1,56 @@
-//package com.demo.test;
-//
-//import org.apache.flink.api.common.functions.RichMapFunction;
-//import org.apache.flink.api.common.restartstrategy.RestartStrategies;
-//import org.apache.flink.api.common.serialization.SimpleStringSchema;
-//import org.apache.flink.configuration.Configuration;
-//import org.apache.flink.runtime.jobgraph.JobVertex;
-//import org.apache.flink.streaming.api.datastream.AllWindowedStream;
-//import org.apache.flink.streaming.api.datastream.DataStreamSource;
-//import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-//import org.apache.flink.streaming.api.functions.sink.PrintSinkFunction;
-//import org.apache.flink.streaming.api.windowing.time.Time;
-//import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
-//import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumerBase;
-//
-//import java.io.InputStream;
-//import java.util.Properties;
-//
-//public class KafkaTest {
-//
-//    public static void main(String[] args) throws Exception {
-//        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-////        env.enableCheckpointing(500);
-//        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(5, 50000));
-//
-//        InputStream stream = KafkaTest.class.getResourceAsStream("/conf/conf.properties");
-//        Properties kafkaConfig = new Properties();
-//        kafkaConfig.load(stream);
-//
-//        String topic = kafkaConfig.getProperty("topics");
-//
-//        Properties pros = new Properties();
-//        pros.setProperty("bootstrap.servers", kafkaConfig.getProperty("bootstrap.servers"));
-//        pros.setProperty("group.id", kafkaConfig.getProperty("group.id"));
-//        pros.setProperty("enable.auto.commit", kafkaConfig.getProperty("enable.auto.commit"));
-//
-//        FlinkKafkaConsumer<String> kafkaConsumer = new FlinkKafkaConsumer<String>(topic,
-//                new SimpleStringSchema(), pros);
-//        //写入偏移量
-//        kafkaConsumer.setCommitOffsetsOnCheckpoints(true);
-//
-//        DataStreamSource<String> dataStreamSource = env.addSource(kafkaConsumer);
-//
-//        dataStreamSource.print();
-//        // 同样效果
-////        dataStreamSource.addSink(new PrintSinkFunction<>());
-//
-//        env.execute("Flink add kafka data source");
-//
-//    }
-//
-//
-//
-//
-//}
+package com.demo.test;
+
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.runtime.jobgraph.JobVertex;
+import org.apache.flink.streaming.api.datastream.AllWindowedStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.PrintSinkFunction;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumerBase;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class KafkaTest {
+
+    public static void main(String[] args) throws Exception {
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+//        env.enableCheckpointing(500);
+        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(5, 50000));
+
+        InputStream stream = KafkaTest.class.getResourceAsStream("/conf/conf.properties");
+        Properties kafkaConfig = new Properties();
+        kafkaConfig.load(stream);
+
+        String topic = kafkaConfig.getProperty("topics");
+
+        Properties pros = new Properties();
+        pros.setProperty("bootstrap.servers", kafkaConfig.getProperty("bootstrap.servers"));
+        pros.setProperty("group.id", kafkaConfig.getProperty("group.id"));
+        pros.setProperty("enable.auto.commit", kafkaConfig.getProperty("enable.auto.commit"));
+
+        FlinkKafkaConsumer<String> kafkaConsumer = new FlinkKafkaConsumer<String>(topic,
+                new SimpleStringSchema(), pros);
+        //写入偏移量
+        kafkaConsumer.setCommitOffsetsOnCheckpoints(true);
+
+        DataStreamSource<String> dataStreamSource = env.addSource(kafkaConsumer);
+
+        dataStreamSource.print();
+        // 同样效果
+//        dataStreamSource.addSink(new PrintSinkFunction<>());
+
+        env.execute("Flink add kafka data source");
+
+    }
+
+
+
+
+}
