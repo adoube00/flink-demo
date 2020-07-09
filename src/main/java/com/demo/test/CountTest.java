b85469052ef6461c47a4133e18a969f9445a60c2
adoube00 <1161524229@qq.com>
2020-07-09 15:46:17 +0800

    时间窗口

---
+++ src/main/java/com/demo/test/CountTest.java
@@ -0,0 +1,73 @@
+package com.demo.test;
+
+import com.demo.utils.ConfigUtil;
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.api.datastream.AllWindowedStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+
+import java.io.InputStream;
+import java.util.Properties;
+//单个时间窗口求sum，无数据输入时不会触发计算
+public class CountTest {
+
+    public static void main(String[] args) throws Exception {
+
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+//        env.enableCheckpointing(500);
+        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(5, 50000));
+
+        InputStream stream = KafkaTest.class.getResourceAsStream("/conf/conf.properties");
+        Properties kafkaConfig = new Properties();
+        kafkaConfig.load(stream);
+
+        String topic = kafkaConfig.getProperty("topics");
+
+        Properties pros = new Properties();
+        pros.setProperty("bootstrap.servers", kafkaConfig.getProperty("bootstrap.servers"));
+        pros.setProperty("group.id", kafkaConfig.getProperty("group.id"));
+        pros.setProperty("enable.auto.commit", kafkaConfig.getProperty("enable.auto.commit"));
+//        Properties pros = ConfigUtil.getKafkaConfig();
+        FlinkKafkaConsumer<String> kafkaConsumer = new FlinkKafkaConsumer<String>(topic,
+                new SimpleStringSchema(), pros);
+        //写入偏移量
+        kafkaConsumer.setCommitOffsetsOnCheckpoints(true);
+
+//        DataStreamSource dataStreamSource = env.addSource(kafkaConsumer);
+        AllWindowedStream<Integer, TimeWindow> stream1 =env
+                .addSource(kafkaConsumer)
+                .map(new String2Integer())
+                .timeWindowAll(Time.seconds(5));
+        SingleOutputStreamOperator<Integer> sum = stream1.sum(0);
+        sum.print();
+
+//        dataStreamSource.print();
+        // 同样效果
+//        dataStreamSource.addSink(new PrintSinkFunction<>());
+
+        env.execute("Flink add kafka data source");
+
+    }
+
+    private static class String2Integer extends RichMapFunction<String,Integer>{
+
+        @Override
+        public Integer map(String val) throws Exception {
+
+            if(null==val){
+                return 0;
+            }
+
+            return Integer.valueOf(val);
+        }
+
+
+    }
+
+}
--- src/main/java/com/demo/utils/ConfigUtil.java
+++ src/main/java/com/demo/utils/ConfigUtil.java
@@ -1,4 +1,25 @@
 package com.demo.utils;
 
-public class ConfigUtil {
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class  ConfigUtil {
+
+
+    public static Properties getKafkaConfig() throws IOException {
+        InputStream stream = ConfigUtil.class.getResourceAsStream("/conf/conf.properties");
+        Properties kafkaConfig = new Properties();
+        kafkaConfig.load(stream);
+
+        String topic = kafkaConfig.getProperty("topics");
+
+        Properties pros = new Properties();
+        pros.setProperty("topic", kafkaConfig.getProperty("topics"));
+        pros.setProperty("bootstrap.servers", kafkaConfig.getProperty("bootstrap.servers"));
+        pros.setProperty("group.id", kafkaConfig.getProperty("group.id"));
+        pros.setProperty("enable.auto.commit", kafkaConfig.getProperty("enable.auto.commit"));
+        return pros;
+    }
+
 }
